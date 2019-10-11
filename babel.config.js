ead8ce4ad2e35610ed66f22664d121670cdb20ef
Pavel Surinin <psurinin@exigengroup.com>
2019-10-11 08:48:23 +0300

    es5 support

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const presets = [
+    [
+        "@babel/env",
+        {
+            targets: {
+                ie: 11
+            },
+            useBuiltIns: "usage",
+        },
+    ]
+];
+
+module.exports = { presets };
